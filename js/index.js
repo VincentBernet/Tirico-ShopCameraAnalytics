c4414cf4902b832543bef27e0944a7712f1e7da9
Vincent Bernet <vincent.bernet99@gmail.com>
2021-01-16 16:11:23 +0100

    Rename Python File, reorganizing tree structure, add logo and Ressource directory, header clean change

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const { Console } = require('console')
+const electron = require('electron')
+const path = require('path');
+const { createLogoutWindow } = require('../src/Authentification/auth-process');
+const { logout } = require('../src/Authentification/auth-service');
+const {BrowserWindow} = electron.remote
+
+
+
+const newWindowBtn = document.getElementById('name')
+
+newWindowBtn.addEventListener('click', (event) => {
+  let win = new BrowserWindow({ width: 400, height: 320 })
+
+  win.on('close', () => { win = null })
+  win.loadURL(modalPath)
+  win.show()
+})
+
+const DeconnectBtn = document.getElementById('DECO')
+
+DeconnectBtn.addEventListener('click', (event) => {
+    logout()
+    console.log("deco");
+    createLogoutWindow();
+    electron.remote.getCurrentWindow().close();
+})
+
+const notification = {
+    title: 'TIRICO Notification',
+    body: 'TIRICO APPLICATION'
+}
+
+const NotifBtn = document.getElementById('NOTIFICATION')
+
+NotifBtn.addEventListener('click', (event) => {
+    const myNotification = new window.Notification(notification.title, notification)
+})
