5c806848129bf40b9dda19d065175337c36049f8
Vincent Bernet <vincent.bernet99@gmail.com>
2021-01-24 15:50:53 +0100

    On appelle le code Python

--- src/Authentification/app-process.js
+++ src/Authentification/app-process.js
@@ -11,7 +11,10 @@ function createWindow () {
     }
   })
   console.log("On a cree une fenetre de app process");
-
+  var python = require('child_process').spawn('python', ['./python/graph.py']);
+  python.stdout.on('data',function(data){
+      console.log("data: ",data.toString('utf8'));
+  });
   win.loadFile('html/index.html');
 
   win.on("closed", () => {
