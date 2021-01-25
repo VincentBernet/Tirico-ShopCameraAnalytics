934f5de2f9b3b1c2254bdbf2c7cd1c6ac7ebe6a2
Vincent Bernet <vincent.bernet99@gmail.com>
2021-01-25 11:51:33 +0100

    Ajout de la heatMap, les boutons pour changer dynamiquement de graphique sont fonctionnels

--- code_python/graph.py
+++ code_python/graph.py
@@ -13,8 +13,22 @@ df['time'].unique()
 df['date'].unique()
 x =df['time'].value_counts().index
 y =df['time'].value_counts().values
-fig = px.bar(df, x, y, title='Affluence/heure')
+fig = px.bar(df, x , y, title='Affluence/heure',
+            labels={
+                     "y": "Nombre de badeaux",
+                     "x": "Horraire"
+                 },)
+fig.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    paper_bgcolor="rgba(0,0,0,0)",
+    font_family="Taoma",
+    font_color="white",
+    title_font_family="Taoma",
+    title_font_color="white",
+    title_font_size=30,
+    legend_title_font_color="green")
 fig.write_image(file='Affluence.png', format='.png')
+
 
 
 
