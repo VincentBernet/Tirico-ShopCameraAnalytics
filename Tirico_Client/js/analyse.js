e0e1ea704fc36c09990855ef320f2bf114dd44be
Vincent Bernet <vincent.bernet99@gmail.com>
2021-04-10 11:58:05 +0200

    Analyse Page Update, need to call Jean's script, hope he select a specific loc.

--- Tirico_Client/js/analyse.js
+++ Tirico_Client/js/analyse.js
@@ -2,6 +2,7 @@
 var mysql = require('mysql');
 
 // Redirection Index ou Inscript
+
 document.getElementById("redirect_Index").addEventListener("click", function() {
     window.location.href="index.html?Name="+Account_Name+"&ID="+Account_ID+"";
 });
