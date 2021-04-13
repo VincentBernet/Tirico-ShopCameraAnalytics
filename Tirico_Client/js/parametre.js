601734e5565829044e752cdcc49fa61c49dee569
Sébastien <sebastien.friedberg@gmail.com>
2021-04-13 19:28:40 +0200

    Correction du layout

--- Tirico_Client/js/parametre.js
+++ Tirico_Client/js/parametre.js
@@ -9,7 +9,7 @@
 document.getElementById("redirect_Analyse").addEventListener("click", function() {
     window.location.href="analyse.html?Name="+Account_Name+"&ID="+Account_ID+"";
 });
-document.getElementById("redirect_Index").addEventListener("click", function() {
+document.getElementById("redirect_Dashboard").addEventListener("click", function() {
     window.location.href="index.html?Name="+Account_Name+"&ID="+Account_ID+"";
 });
 document.getElementById("redirect_Disconnection").addEventListener("click", function() {
