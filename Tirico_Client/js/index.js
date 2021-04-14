b883b584937979b38762cb1a22545be6b8c06c8b
Vincent Bernet <vincent.bernet99@gmail.com>
2021-04-14 16:09:26 +0200

    Update

--- Tirico_Client/js/index.js
+++ Tirico_Client/js/index.js
@@ -25,6 +25,9 @@ new customTitlebar.Titlebar({
 });
 
 // Redirection Index ou Inscript
+document.getElementById("redirect_Index").addEventListener("click", function() {
+    window.location.href="index.html?Name="+Account_Name+"&ID="+Account_ID+"";
+});
 document.getElementById("redirect_Analyse").addEventListener("click", function() {
     window.location.href="analyse.html?Name="+Account_Name+"&ID="+Account_ID+"";
 });
--- Tirico_Client/js/parametre.js
+++ Tirico_Client/js/parametre.js
@@ -1,16 +1,24 @@
 
-
+// We take back the paramater of the ID, we parse it cleany first
+// We take back the paramater of the ID, we parse it cleany first
+const urlData = window.location.search;
+const urlParams = new URLSearchParams(urlData);
+const Account_ID = urlParams.get('ID');
+const Account_Name = urlParams.get('Name');
 
 
 
 
 
 // Redirection Index ou Inscript
+document.getElementById("redirect_Index").addEventListener("click", function() {
+    window.location.href="index.html?Name="+Account_Name+"&ID="+Account_ID+"";
+});
 document.getElementById("redirect_Analyse").addEventListener("click", function() {
     window.location.href="analyse.html?Name="+Account_Name+"&ID="+Account_ID+"";
 });
-document.getElementById("redirect_Index").addEventListener("click", function() {
-    window.location.href="index.html?Name="+Account_Name+"&ID="+Account_ID+"";
+document.getElementById("redirect_Parameter").addEventListener("click", function() {
+    window.location.href="parametre.html?Name="+Account_Name+"&ID="+Account_ID+"";
 });
 document.getElementById("redirect_Disconnection").addEventListener("click", function() {
     window.location.href="connexion.html";
