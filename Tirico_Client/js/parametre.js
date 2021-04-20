e0f1d7b2f3805d101873b906ea74e61e707949ef
Vincent Bernet <vincent.bernet99@gmail.com>
2021-04-20 15:30:40 +0200

    Better Parameter, when Selection Zone btn is Select call Serge's Script

--- Tirico_Client/js/parametre.js
+++ Tirico_Client/js/parametre.js
@@ -228,38 +228,443 @@ Get_Datas("NbVente").then(function(dat) {
   MakeCercle();
 }).catch((err) => setImmediate(() => { throw err; }));`;
 
-var Code4 = `Get_Datas("NombreDePassage").then(function(dat) {
-  dat.forEach(function(item, index, array) 
-  {
-      datas.push(item.NombreDePassage);
-      Graphe_Label.push(item.DateTime.getHours() + 'h');
-  })
-  titre[0] = "Affluence client par jour";
-  MakeLineGraph();
-  MakeBar();
-}).catch((err) => setImmediate(() => { throw err; }));`;
+var Code4 = `var graph1;
+function Affluence_LineGraph() {
+    if (graph1 != null)
+    {
+        graph1.destroy();
+    }
+
+    console.log(datas.length);
+    ctx = document.getElementById('Vente').getContext('2d')
+
+    data = {
+        labels: Graphe_Label,
+        datasets: [{
+            backgroundColor: '#2d9ae0',
+            borderColor: '#2d9ae0',
+            data: datas,
+            label: "Personne"
+        }]
+    };
+    options = {
+        title: {
+            display: true,
+            position: 'top',
+            fontSize: '18',
+            fontFamily: 'Tahoma',
+            fontColor: colorTitre,
+            fontStyle: 'bold',
+            padding: '0',
+            lineHeight: '1.5',
+            text: titre[0]
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInQuad'
+        }
+    };
+
+    graph1 = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options : options
+    });
+}
+var colorTitre = '#fff';
+var graph2; 
+function CA_BarGraph()
+{
+    if (graph2 != null)
+    {
+        graph2.destroy();
+    }
+    ctx = document.getElementById('graph5').getContext('2d')
+    data = {
+        labels: Graphe_Label,
+        datasets: [{
+            backgroundColor: '#FFC300',
+            hoverBackgroundColor: '#fff',
+            hoverBorderWidth: '#fff',
+            borderColor: 'rgb(255, 99, 132)',
+            label: "Chiffre d'affaires en euros (€)",
+            data: datas_CA
+        }]
+    };
+    options = {
+        title: {
+            display: true,
+            position: 'top',
+            fontSize: '18',
+            fontFamily: 'Tahoma',
+            fontColor: colorTitre,
+            fontStyle: 'bold',
+            padding: '0',
+            lineHeight: '1.5',
+            text: titre[2],
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInQuad'
+        }
+    };
+    graph2 = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+}
+
+
+var graphVente;
+function MakeVenteLineGraph() {
+    if (graphVente != null)
+    {
+        graphVente.destroy();
+    }
+    ctx = document.getElementById('graph3').getContext('2d')
+
+    data = {
+        labels: Graphe_Label,
+        datasets: [{
+            backgroundColor: '#900C3F',
+            data: datas_ventes,
+            label: "Vente"
+        }]
+    };
+    options = {
+        title: {
+            display: true,
+            position: 'top',
+            fontSize: '18',
+            fontFamily: 'Tahoma',
+            fontColor: colorTitre,
+            fontStyle:'bold',
+            padding: '0',
+            lineHeight: '1.5',
+            text: titre[1]
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInQuad'
+        }
+    };
+
+    graphVente = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options : options
+    });
+}
+
+var graphVenteBar; 
+function MakeBarVente()
+{
+    if (graphVenteBar != null)
+    {
+        graphVenteBar.destroy();
+    }
+    ctx = document.getElementById('graph6').getContext('2d')
+
+    data = {
+        labels: Graphe_Label,
+        datasets: [{
+            backgroundColor: '#2d9ae0',
+            hoverBackgroundColor: '#fff',
+            hoverBorderWidth: '#fff',
+            borderColor: 'rgb(255, 99, 132)',
+            label: 'Nombre de personne',
+            data: datas
+        },
+        {
+            backgroundColor: '#900C3F',
+            hoverBackgroundColor: '#fff',
+            hoverBorderWidth: '#fff',
+            borderColor: 'rgb(255, 99, 132)',
+            label: 'Nombre de produits vendus',
+            data: datas_ventes
+        },
+        {
+            backgroundColor: '#FFC300',
+            hoverBackgroundColor: '#fff',
+            hoverBorderWidth: '#fff',
+            borderColor: 'rgb(255, 99, 132)',
+            label: "Chiffre d'affaires",
+            data: datas_CA
+        }
+    ]
+    };
+    options = {
+        title: {
+            display: true,
+            position: 'top',
+            fontSize: '18',
+            fontFamily: 'Tahoma',
+            fontColor: colorTitre,
+            fontStyle:'bold',
+            padding: '0',
+            lineHeight: '1.5',
+            text: "Every  graphics together",
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInQuad'
+        }
+    };
+    graphVenteBar = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+}
+
+var graph5;
+function MakeCercle() {
+
+    var t = [];
+    datas.forEach(function(item, index, array) {
+        t.push('rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255 + ')');
+    })
+
+
+    if (graph5 != null)
+    {
+        graph5.destroy();
+    }
+    ctx = document.getElementById('graph2').getContext('2d')
+
+    data = {
+        labels: Graphe_Label,
+        datasets: [{
+            backgroundColor: t,
+            borderColor: '#0000',
+            data: datas
+        }]
+    }
+
+    options = {
+        title: {
+            display: true,
+            position: 'top',
+            fontSize: '18',
+            fontFamily: 'Tahoma',
+            fontColor: colorTitre,
+            fontStyle:'bold',
+            padding: '0',
+            lineHeight: '1.5',
+            text: titre[0]
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInQuad'
+        }
+    }
+
+    graph5 = new Chart(ctx, {
+    type: 'polarArea',
+    data: data,
+    options: options
+});
+}
+
+var graph6;
+function MakeAraignee() 
+{
+    if (graph6 != null)
+    {
+        graph6.destroy();
+    }
+    ctx = document.getElementById('graph4').getContext('2d')
+
+    data = {
+        label: 'Vente',
+    labels: Graphe_Label,
+    datasets: [{
+        label: titre[0],
+        backgroundColor: '#900C3F',
+        borderColor: '#44061E',
+        fill: true,
+        pointBorderColor: '#fff',
+        pointBackgroundColor: '#900C3F',
+        data: datas
+    }]
+}
+    options = {
+        title: {
+            display: true,
+            position: 'top',
+            fontSize: '18',
+            fontFamily: 'Tahoma',
+            fontColor: colorTitre,
+            fontStyle:'bold',
+            padding: '0',
+            lineHeight: '1.5',
+            text: titre[1]
+        },
+            animation: {
+                duration: 1000,
+                easing: 'easeInQuad'
+            },
+            scale: {
+            }
+        }
+
+    graph6 = new Chart(ctx, {
+        type: 'radar',
+        data: data,
+        options: options
+    });
+
+}`;
+
+var Code5 = CodeNbrVente;
+
+var Code6 = `# Conseil et Prediction journalier Script Python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import mysql.connector
+import datetime
+import sys, json
+from datetime import datetime
+
+#Code to Read data parse to this script
+#Read data from stdin
+lines = sys.stdin.readlines()
+# Since our input would only be having one line, parse our JSON data from that
+#get our data as an array from read_in()
+lines = json.loads(lines[0])
+
+print (lines[1])
+IdObject = lines[0]
+
+
+
+
+db = mysql.connector.connect(
+  host="mysql-pa8.alwaysdata.net",
+  user="pa8_acc",
+  password="5wtE3Cx8W",
+  database="pa8_bddv2",)
+
+
+# In[2]:
+
+
+def  Création_liste():
+    maxi = 0
+    tab_1 = []
+    heure = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    h = 3600
+    jour = 3600*24
+    semaine = 3600*24*7
+    for k in range (0,10):
+        tab_1 = []
+        for i in range (0,24):
+            
+    
+            cursor = db.cursor()
+            Vdate = datetime.datetime.fromtimestamp(1577836800 + (h*i) +(jour*k)+ (semaine*0))
+            cursor.execute("""SELECT NombreDePassage FROM Stats JOIN StatsToLoc on Stats.ID = StatsToLoc.IDStats WHERE IDLoc = %s AND DateTime = %s""", ( 1 ,Vdate, ))
+            a = cursor.fetchone()
+            a = a[0]
+            
+            tab_1.append(a)
+            
+        
+        maxi = tab_1[0]
+        for y in tab_1:
+            if y >= maxi:
+                maxi = y
+        
+        for p in range (0,24):
+            if p == tab_1.index(maxi) : heure[p] = (heure[p]+1)
+                
+        
+    
+    maximum = tab_1[0]
+    for e in tab_1:
+        if e >= maximum:
+            maximum = e
+    print("l'heure a laquelle le pic de clientèle sera observé demain",  (tab_1.index(maximum)+1))
+
+
+# In[3]:
+
+
+def coef_event():
+    coef_event1 = 1
+    p= datetime.now()
+    print(p)
+coef_event()
+
+
+# In[4]:
+
+
+def Conseil_stocks( coef_event):
+    ID = 100
+    
+    print ("Object Sélectionner: ",IdObject)
+    ID = int(IdObject)
+    cursor = db.cursor()
+    cursor.execute("""SELECT Nom FROM Item  WHERE ID = %s """, ( ID , ))
+    objet = cursor.fetchone()
+    objet = objet [0]
+    
+    cursor.execute("""SELECT Vente_mois FROM Item  WHERE ID = %s """, ( ID , ))
+    moy_mois = cursor.fetchone()
+    moy_mois = moy_mois[0]
+    
+    cursor.execute("""SELECT Vente_troisj FROM Item  WHERE ID = %s """, ( ID , ))
+    moy_trois_jours = cursor.fetchone()
+    moy_trois_jours = moy_trois_jours[0]
+    
+    valeur_stock = ((moy_mois)-(moy_trois_jours))*(coef_event)
+    if valeur_stock<0.90 : print("Il faut augmenter le stock en prevision d'une hausse de consommation future de ",objet,".")
+    elif 0.90<valeur_stock<1.10 : print("Il faut continuer d'alimenter les stocks de ", objet," de manière constante, rien ne devrait bouger pour le produit.")
+    elif valeur_stock>1.10 : print ("Il faut cesser d'alimenter les stocks de ", objet ," ce produit sera en perte de vitesse sur les prochains jours.")
+
+#Création_liste()
+Conseil_stocks(1)
+# In[ ]:
+
+
+`;
 
 // Appel Python du code de sélection de zone, ouvre une nouvelle Window
-let pyshell = new PythonShell('code_python/DrawZone/drawShopZones.py');
+const SelectionZoneBtn = document.getElementById('selection_Zone')
+    SelectionZoneBtn.addEventListener('click', (event) => {
+    let pyshell = new PythonShell('code_python/DrawZone/drawShopZones.py');
 
-pyshell.send(JSON.stringify(['\n-----------------------------------------------------\nAppel du script de Selection de Zone "drawShopZones.py"']))
+    pyshell.send(JSON.stringify(['\n-----------------------------------------------------\nAppel du script de Selection de Zone "drawShopZones.py"']))
 
-pyshell.on('message', function(message) {
-  console.log(message);
-})
+    pyshell.on('message', function(message) {
+    console.log(message);
+    })
 
-pyshell.end(function (err) {
-  if (err){
-    throw err;
-  };
-  console.log('Fin du script Python : "drawShopZones.py"');
-  console.log("-----------------------------------------------------");
+    pyshell.end(function (err) {
+        if (err){
+            throw err;
+    };
+    console.log('Fin du script Python : "drawShopZones.py"');
+    console.log("-----------------------------------------------------");
+    });
 });
+
+
+
 
 const redirectInscriBtn = document.getElementById("redirect_InscriptionLoc")
 redirectInscriBtn.addEventListener('click', (event) => {
   window.location.href="inscription_loc.html?Name="+Account_Name+"&ID="+Account_ID+"";;
-})
+});
 
 //Script pour modifier le code source souhaiter selon le "select" sélectionner + Alert si le boutton restaurer ou valider est activé
 
@@ -273,7 +678,7 @@ redirectInscriBtn.addEventListener('click', (event) => {
   ValidateBtn.addEventListener('click', (event) => {
             var graphSelection = document.getElementById("Graph_Selected")
             var Graph_Selected = graphSelection.value
-            alert("Le graphe d'"+Graph_Selected+" a bien été modifié")
+            alert("Le graphe '"+Graph_Selected+"' a bien été modifié")
         })
 
 
@@ -282,21 +687,24 @@ redirectInscriBtn.addEventListener('click', (event) => {
             var graphSelection = document.getElementById("Graph_Selected")
             var Graph_Selected = graphSelection.value
             switch (Graph_Selected) {
-              case '1':
+              case 'HeatMap':
                 var Code_To_Modify = CodeHeatmap;
                 break;
-              case '2':
+              case 'Affluence Graphe':
                 var Code_To_Modify = CodeAffluence;
                 break;
-              case '3':
+              case 'Ventes Produit Graphe':
                 var Code_To_Modify = CodeNbrVente;
                 break;
-              case '4':
+              case 'Chiffre affaire Graphe':
                 var Code_To_Modify = Code4;
                 break;
-              case '5':
+              case 'Multi Graphe':
                 var Code_To_Modify = Code5;
                   break;
+              case 'Conseil Journalier':
+                  var Code_To_Modify = Code6;
+                    break;
               default:
                 Code_To_Modify = "Error";
             }
